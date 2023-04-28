87e075b6d5152b76c1cc4caa50663e87ac772bde
shiro019 <shiro019@umn.edu>
2023-04-28 05:58:37 -0500

    implement left motor and driving directions

--- autonomous-vehicle/micro-ros/src/main.c
+++ autonomous-vehicle/micro-ros/src/main.c
@@ -17,15 +17,54 @@
 #define BDC_MCPWM_GPIO_A              33
 #define BDC_MCPWM_GPIO_B              25
 
+/* Motor 2 pins */
+#define BDC_MCPWM_GPIO_C              32
+#define BDC_MCPWM_GPIO_D              26
+
+
 static const char *TAG = "example";   //for prints
+
+void explorer_set_speed(bdc_motor_handle_t left_motor, bdc_motor_handle_t right_motor, uint16_t speed)
+{
+    ESP_LOGI(TAG, "Setting speed...");
+    bdc_motor_set_speed(right_motor, (uint32_t)speed);
+    bdc_motor_set_speed(left_motor, (uint32_t)speed);
+}
+
+void explorer_set_left(bdc_motor_handle_t left_motor, bdc_motor_handle_t right_motor)
+{
+    ESP_LOGI(TAG, "Forward: right - Reverse: left");
+    ESP_ERROR_CHECK(bdc_motor_forward(right_motor));
+    ESP_ERROR_CHECK(bdc_motor_reverse(left_motor));
+}
+
+void explorer_set_right(bdc_motor_handle_t left_motor, bdc_motor_handle_t right_motor)
+{
+    ESP_LOGI(TAG, "Forward: left - Reverse: right");
+    ESP_ERROR_CHECK(bdc_motor_forward(left_motor));
+    ESP_ERROR_CHECK(bdc_motor_reverse(right_motor));
+}
+
+void explorer_set_reverse(bdc_motor_handle_t left_motor, bdc_motor_handle_t right_motor)
+{
+    ESP_LOGI(TAG, "Forward: none - Reverse: left, right");
+    ESP_ERROR_CHECK(bdc_motor_reverse(left_motor));
+    ESP_ERROR_CHECK(bdc_motor_reverse(right_motor));
+}
+
+void explorer_set_forward(bdc_motor_handle_t left_motor, bdc_motor_handle_t right_motor)
+{
+    ESP_LOGI(TAG, "Forward: left, right - Reverse: none");
+    ESP_ERROR_CHECK(bdc_motor_forward(left_motor));
+    ESP_ERROR_CHECK(bdc_motor_forward(right_motor));
+}
+
+void explorer_drive_ms(bdc_motor_handle_t left_motor, bdc_motor_handle_t right_motor, uint32_t time){
+    vTaskDelay(pdMS_TO_TICKS(time));
+}
 
 static void mcpwm_bdc(void *arg){
     /* DC motor config */
-    bdc_motor_config_t motor_config = {         //TO-DO: make init a function to config multiple motors
-        .pwm_freq_hz = BDC_MCPWM_FREQ_HZ,
-        .pwma_gpio_num = BDC_MCPWM_GPIO_A,
-        .pwmb_gpio_num = BDC_MCPWM_GPIO_B,
-    };
     bdc_motor_mcpwm_config_t mcpwm_config = {
         .group_id = 0,
         .resolution_hz = BDC_MCPWM_TIMER_RESOLUTION_HZ,
@@ -35,21 +74,35 @@ static void mcpwm_bdc(void *arg){
 
     /* right motor handle */
     bdc_motor_handle_t right_motor = NULL;
-    ESP_ERROR_CHECK(bdc_motor_new_mcpwm_device(&motor_config, &mcpwm_config, &right_motor));
+    bdc_motor_config_t right_motor_config = {
+        .pwm_freq_hz = BDC_MCPWM_FREQ_HZ,
+        .pwma_gpio_num = BDC_MCPWM_GPIO_A,
+        .pwmb_gpio_num = BDC_MCPWM_GPIO_B,
+    };
+    ESP_ERROR_CHECK(bdc_motor_new_mcpwm_device(&right_motor_config, &mcpwm_config, &right_motor));
     bdc_motor_set_speed(right_motor, (uint32_t)BDC_INIT_SPEED);
     ESP_LOGI(TAG, "Enable right_motor");
     ESP_ERROR_CHECK(bdc_motor_enable(right_motor));
 
+    /* left motor handle */
+    bdc_motor_handle_t left_motor = NULL;
+    bdc_motor_config_t left_motor_config = {
+        .pwm_freq_hz = BDC_MCPWM_FREQ_HZ,
+        .pwma_gpio_num = BDC_MCPWM_GPIO_C,
+        .pwmb_gpio_num = BDC_MCPWM_GPIO_D,
+    };
+    ESP_ERROR_CHECK(bdc_motor_new_mcpwm_device(&left_motor_config, &mcpwm_config, &left_motor));
+    bdc_motor_set_speed(left_motor, (uint32_t)BDC_INIT_SPEED);
+    ESP_LOGI(TAG, "Enable left_motor");
+    ESP_ERROR_CHECK(bdc_motor_enable(left_motor));
+
     while (1) {
-        bdc_motor_set_speed(right_motor, (uint32_t)BDC_MIN_SPEED);
+        uint16_t speed = BDC_MIN_SPEED;
+        uint32_t drive_time_ms = 500;
 
-        ESP_LOGI(TAG, "Forward right_motor");
-        ESP_ERROR_CHECK(bdc_motor_forward(right_motor));
-        vTaskDelay(pdMS_TO_TICKS(500));
-
-        ESP_LOGI(TAG, "Reverse right_motor");
-        ESP_ERROR_CHECK(bdc_motor_reverse(right_motor));
-        vTaskDelay(pdMS_TO_TICKS(500));
+        explorer_set_speed(left_motor, right_motor, speed);
+        explorer_set_forward(left_motor, right_motor);
+        explorer_drive_ms(left_motor, right_motor, drive_time_ms);
     }
 }
 
