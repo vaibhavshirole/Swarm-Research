ee59f8e0d4a553e46a350af46c89e8c72be39fd1
shiro019 <shiro019@umn.edu>
2023-04-28 04:42:07 -0500

    version with deprecated mcpwm lib dep

--- autonomous-vehicle/micro-ros/src/main.c
+++ autonomous-vehicle/micro-ros/src/main.c
@@ -1,60 +1,80 @@
-#include <stdio.h>
-#include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/queue.h"
-#include "esp_log.h"
-#include "bdc_motor.h"
+#include "driver/gpio.h"
+#include "esp_attr.h"
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
 
-/* General BDC motor defines */
-#define BDC_MCPWM_TIMER_RESOLUTION_HZ 10000000 // 10MHz, 1 tick = 0.1us
-#define BDC_MCPWM_FREQ_HZ             25000    // 25KHz PWM
-#define BDC_MCPWM_DUTY_TICK_MAX       (BDC_MCPWM_TIMER_RESOLUTION_HZ / BDC_MCPWM_FREQ_HZ) // maximum value we can set for the duty cycle, in ticks
-#define BDC_INIT_SPEED                0        //speed can be 0-400
-#define BDC_MIN_SPEED                 200
+#define GPIO_PWM0A_OUT 33   //Set GPIO 15 as PWM0A
+#define GPIO_PWM0B_OUT 25   //Set GPIO 16 as PWM0B
 
-/* Motor 1 pins */
-#define BDC_MCPWM_GPIO_A              33
-#define BDC_MCPWM_GPIO_B              25
-
-static const char *TAG = "example";   //for prints
-
-static void mcpwm_bdc(void *arg){
-    /* DC motor config */
-    bdc_motor_config_t motor_config = {         //TO-DO: make init a function to config multiple motors
-        .pwm_freq_hz = BDC_MCPWM_FREQ_HZ,
-        .pwma_gpio_num = BDC_MCPWM_GPIO_A,
-        .pwmb_gpio_num = BDC_MCPWM_GPIO_B,
-    };
-    bdc_motor_mcpwm_config_t mcpwm_config = {
-        .group_id = 0,
-        .resolution_hz = BDC_MCPWM_TIMER_RESOLUTION_HZ,
-    };
-
-    ESP_LOGI(TAG, "Create DC motors");
-
-    /* right motor handle */
-    bdc_motor_handle_t right_motor = NULL;
-    ESP_ERROR_CHECK(bdc_motor_new_mcpwm_device(&motor_config, &mcpwm_config, &right_motor));
-    bdc_motor_set_speed(right_motor, (uint32_t)BDC_INIT_SPEED);
-    ESP_LOGI(TAG, "Enable right_motor");
-    ESP_ERROR_CHECK(bdc_motor_enable(right_motor));
-
-    while (1) {
-        bdc_motor_set_speed(right_motor, (uint32_t)BDC_MIN_SPEED);
-
-        ESP_LOGI(TAG, "Forward right_motor");
-        ESP_ERROR_CHECK(bdc_motor_forward(right_motor));
-        vTaskDelay(pdMS_TO_TICKS(500));
-
-        ESP_LOGI(TAG, "Reverse right_motor");
-        ESP_ERROR_CHECK(bdc_motor_reverse(right_motor));
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
+static void mcpwm_example_gpio_initialize(void)
+{
+    printf("initializing mcpwm gpio...\n");
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, GPIO_PWM0A_OUT);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, GPIO_PWM0B_OUT);
 }
-
+/**
+ * @brief motor moves in forward direction, with duty cycle = duty %
+ */
+static void brushed_motor_forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle)
+{
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
+    mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, duty_cycle);
+    mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_A, MCPWM_DUTY_MODE_0); //call this each time, if operator was previously in low/high state
+}
+/**
+ * @brief motor moves in backward direction, with duty cycle = duty %
+ */
+static void brushed_motor_backward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle)
+{
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);
+    mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, duty_cycle);
+    mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_B, MCPWM_DUTY_MODE_0);  //call this each time, if operator was previously in low/high state
+}
+/**
+ * @brief motor stop
+ */
+static void brushed_motor_stop(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num)
+{
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
+}
+/**
+ * @brief Configure MCPWM module for brushed dc motor
+ */
+static void mcpwm_example_brushed_motor_control(void *arg)
+{
+    //1. mcpwm gpio initialization
+    mcpwm_example_gpio_initialize();
+    //2. initial mcpwm configuration
+    printf("Configuring Initial Parameters of mcpwm...\n");
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = 1000;    //frequency = 500Hz,
+    pwm_config.cmpr_a = 200;    //duty cycle of PWMxA = 0
+    pwm_config.cmpr_b = 200;    //duty cycle of PWMxb = 0
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);    //Configure PWM0A & PWM0B with above settings
+    
+    brushed_motor_forward(MCPWM_UNIT_0, MCPWM_TIMER_0, 200);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    brushed_motor_backward(MCPWM_UNIT_0, MCPWM_TIMER_0, 200);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    brushed_motor_stop(MCPWM_UNIT_0, MCPWM_TIMER_0);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    
+while(1){
+    brushed_motor_forward(MCPWM_UNIT_0, MCPWM_TIMER_0, 200);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    brushed_motor_backward(MCPWM_UNIT_0, MCPWM_TIMER_0, 200);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    brushed_motor_stop(MCPWM_UNIT_0, MCPWM_TIMER_0);
+    vTaskDelay(pdMS_TO_TICKS(1500));
+}
+}
 void app_main(void)
 {
-    ESP_LOGI(TAG, "Running task...");
-    xTaskCreate(mcpwm_bdc, "mcpwm_bdc", 4096, NULL, 5, NULL);
+    printf("Testing brushed motor...\n");
+    xTaskCreate(mcpwm_example_brushed_motor_control, "mcpwm_example_brushed_motor_control", 4096, NULL, 5, NULL);
 }
